74826a91f2519fe370f6023c859823fd06a862b7
theusknupp <matheus.knupp.ds@outlook.com>
2024-05-01 15:13:23 -0300

    User entity and resources

---
+++ src/main/java/com/estudo/course/resources/UserResources.java
@@ -0,0 +1,20 @@
+package com.estudo.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudo.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users") //Controlador rest que responde no caminho users
+public class UserResources {
+
+	@GetMapping //Requisição do tipo get do http
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "Maria@gmail.com", "993131", "12345");
+		return ResponseEntity.ok().body(u); //.ok para voltar resposta com sucesso, .body para retornar corpo da resposta instanciado
+	}
+	
+}
