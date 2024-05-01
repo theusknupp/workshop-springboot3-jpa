f8e161252630339d9d164a10a7d415aa52942de0
theusknupp <matheus.knupp.ds@outlook.com>
2024-05-01 15:46:22 -0300

    H2 database, test profile, JPA

--- src/main/java/com/estudo/course/resources/UserResources.java
+++ src/main/java/com/estudo/course/resources/UserResources.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.estudo.course.entities.User;
 
 @RestController
-@RequestMapping(value = "/users") //Controlador rest que responde no caminho users
+@RequestMapping(value = "/users")  //Controlador rest que responde no caminho users
 public class UserResources {
 
-	@GetMapping //Requisição do tipo get do http
+	@GetMapping //get http
 	public ResponseEntity<User> findAll() {
 		User u = new User(1L, "Maria", "Maria@gmail.com", "993131", "12345");
 		return ResponseEntity.ok().body(u); //.ok para voltar resposta com sucesso, .body para retornar corpo da resposta instanciado
