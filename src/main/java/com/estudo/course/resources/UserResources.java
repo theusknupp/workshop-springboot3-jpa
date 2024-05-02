4dc75cd6e620f6c96228ff74109aaa436fe3b44d
theusknupp <matheus.knupp.ds@outlook.com>
2024-05-02 17:24:24 -0300

    User delete

--- src/main/java/com/estudo/course/resources/UserResources.java
+++ src/main/java/com/estudo/course/resources/UserResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,4 +43,9 @@ public class UserResources {
 		return ResponseEntity.created(uri).body(obj); //201 Created no postman
 	}
 	
+	@DeleteMapping(value = "/{id}") //Anotação para deletar
+	public ResponseEntity<Void> delete(@PathVariable Long id) { //PathVariable para o long id ser reconhecido como variavel url
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
